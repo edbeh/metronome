6cc28e9fbb3170b96431c35c0aaaba03284d157c
edbeh <edbeh89@gmail.com>
2020-04-17 20:55:20 +0800

    Updates

---
+++ precache-manifest.5fa21f3a18c29f7b03299ffd8304ed18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca284212c51bab9ee9925cfbb3dc478",
+    "url": "/metronome/index.html"
+  },
+  {
+    "revision": "b33ac83d325d717d0a1a",
+    "url": "/metronome/static/css/main.a70406a8.chunk.css"
+  },
+  {
+    "revision": "bae175f33249004328eb",
+    "url": "/metronome/static/js/2.a3567b9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/metronome/static/js/2.a3567b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33ac83d325d717d0a1a",
+    "url": "/metronome/static/js/main.f8fe3eed.chunk.js"
+  },
+  {
+    "revision": "dcb1e3a8a631114de19f",
+    "url": "/metronome/static/js/runtime-main.4238f24b.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/metronome/static/media/click2.fe0ded65.wav"
+  }
+]);
